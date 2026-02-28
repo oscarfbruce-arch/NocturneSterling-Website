8984916d1a19f241513db7076a65666e3bf3e984
oscarfbruce-arch <oscar.f.bruce@gmail.com>
2026-02-28 04:27:17 +0000

    new file:   MoveingBackground.js 	modified:   style.css

---
+++ MoveingBackground.js
@@ -0,0 +1,8 @@
+let step = 0;
+let backHightPx = 20460
+
+setInterval(() => {
+step = (step + 1) % (101)
+  document.documentElement.style.setProperty('step', step + '%');
+  console.log(step);
+}, 100)
