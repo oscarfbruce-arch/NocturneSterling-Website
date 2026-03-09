cba03f8373a68dea5f65a7770fe5cb44f91010a6
oscarfbruce-arch <oscar.f.bruce@gmail.com>
2026-03-09 01:15:53 +0000

    modified:   Links.html 	modified:   LinksDeutsch.html 	modified:   index.html 	modified:   indexDeutsch.html 	modified:   js/MoveingBackground.js 	modified:   misc/README.md 	modified:   not_found.html 	modified:   style.css

--- js/MoveingBackground.js
+++ js/MoveingBackground.js
@@ -1,10 +1,14 @@
-let step = 0;
-let speed = 1;
+sessionStorage.setItem("step",8000);
+let step = Number(sessionStorage.getItem("step")) || 0;
+let speed = 5;
 let delay = 100;
-let backHightPx = 20460
+let backHightPx = 20460;
 
-setInterval(() => {
-step = (step + speed) % (101)
-  document.documentElement.style.setProperty('step', step + '%');
-  console.log("step:" + step);
-}, delay)
+animate() {
+  step = (step + speed) % (backHightPx);
+  sessionStorage.setItem("step",step);
+  document.documentElement.style.setProperty('--step', step + 'px');
+  requestAnimationFrame(animate);
+}
+
+requestAnimationFrame(animate);
